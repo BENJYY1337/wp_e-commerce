cd701ca170cac7e799a1e886ccd541f70803f9a5
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-07-25 13:33:25 +0200

    complete push

---
+++ wordpress/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1596185778;s:7:"authKey";s:64:"4X0kU.rG9>%M*FN>8T^pU_[f0{fObyivHVwKjd8M%]%EPL8V17{XPaV7_F%)G [w";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1502;}
